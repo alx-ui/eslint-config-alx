7eba9d66b431d4261f4bfbea1198763c7d74b35b
ialexanderbrito <xanderzinho26@gmail.com>
2025-12-16 09:50:03 -0300

    refactor: migrate to ES module syntax and enhance ESLint configuration

--- next.js
+++ next.js
@@ -1,106 +1,140 @@
-module.exports = {
-  plugins: ["prettier", "eslint-plugin-import-helpers"],
-  extends: ["next", "next/core-web-vitals", "prettier"],
-  rules: {
-    'prettier/prettier': 'error',
-    'camelcase': 'off',
-    'import/prefer-default-export': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/no-unused-prop-types': 'off',
-    'react/require-default-props': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-    'camelcase': 'off',
-    'semi': 'off',
-    'eqeqeq': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'implicit-arrow-linebreak': 'off',
-    'react/jsx-filename-extension': ['warn', { 'extensions': ['.tsx'] }],
-    'arrow-body-style': ['error', 'as-needed'],
-    'class-methods-use-this': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'react/prop-types': 'off',
-    'react/no-array-index-key': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'no-unused-vars': 'error',
-    'no-nested-ternary': 'error',
-    'no-param-reassign': 'error',
-    'no-unneeded-ternary': 'error',
-    'react/forbid-prop-types': 'off',
-    'react/require-default-props': 'off',
-    'react/destructuring-assignment': 'off',
-    'jsx-a11y/heading-has-content': 'off',
-    'jsx-a11y/alt-text': 'off',
-    'react/jsx-key': 'off',
-    'consistent-return': 'off',
-    'react/no-unescaped-entities': 'off',
-    'jsx-a11y/label-has-associated-control': 'off',
-    'prefer-promise-reject-errors': 'off',
-    'radix': 'off',
-    'react/display-name': 'off',
-    'func-names': 'off',
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        'newlinesBetween': 'always',
-        'groups': [
-          '/^react/',
-          'module',
-          '/^pages/',
-          '/^components/',
-          '/^utils/',
-          '/^contexts/',
-          '/^hooks/',
-          '/^axios/',
-          '/^services/',
-          '/^styles/',
-          ['parent', 'sibling', 'index']
-        ],
-        'alphabetize': { 'order': 'asc', 'ignoreCase': true }
-      }
-    ],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "ts": "never",
-        "tsx": "never",
-        "js": "never",
-        "jsx": "never"
-      }
-    ],
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        "components": ["Link"],
-        "specialLink": ["hrefLeft", "hrefRight"],
-        "aspects": ["invalidHref", "preferButton"]
-      }
-    ]
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginImportX from 'eslint-plugin-import-x';
+import eslintPluginNext from '@next/eslint-plugin-next';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import prettierConfig from './prettier.config.js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginReact.configs.flat['jsx-runtime'],
+  eslintPluginJsxA11y.flatConfigs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      'jsx-a11y': eslintPluginJsxA11y,
+      'import-x': eslintPluginImportX,
+      '@next/next': eslintPluginNext,
+      prettier: eslintPluginPrettier,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs['core-web-vitals'].rules,
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      'prettier/prettier': ['error', prettierConfig],
+      camelcase: 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/no-unused-prop-types': 'off',
+      'react/require-default-props': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+      semi: 'off',
+      eqeqeq: 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.tsx', '.jsx'] }],
+      'arrow-body-style': ['error', 'as-needed'],
+      'class-methods-use-this': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'react/no-array-index-key': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-nested-ternary': 'error',
+      'no-param-reassign': 'error',
+      'no-unneeded-ternary': 'error',
+      'react/forbid-prop-types': 'off',
+      'react/destructuring-assignment': 'off',
+      'jsx-a11y/heading-has-content': 'off',
+      'jsx-a11y/alt-text': 'off',
+      'react/jsx-key': 'off',
+      'consistent-return': 'off',
+      'react/no-unescaped-entities': 'off',
+      'jsx-a11y/label-has-associated-control': 'off',
+      'prefer-promise-reject-errors': 'off',
+      radix: 'off',
+      'react/display-name': 'off',
+      'func-names': 'off',
+      'import-x/order': [
+        'warn',
+        {
+          'newlines-between': 'always',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: 'next/**',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react', 'next'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['invalidHref', 'preferButton'],
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
   },
-  "overrides": [
-    {
-      "files": "**/*.+(ts|tsx)",
-      "parser": "@typescript-eslint/parser",
-      "plugins": ["@typescript-eslint/eslint-plugin"],
-      "extends": ["plugin:@typescript-eslint/recommended", "prettier"],
-      "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "no-use-before-define": [0],
-        "@typescript-eslint/no-use-before-define": [1],
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-var-requires": "off"
-      }
-    }
-  ]
-}
+];
+
